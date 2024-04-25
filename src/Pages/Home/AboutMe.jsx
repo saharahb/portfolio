2bd2a7cbf9adbce1e30b23e9cbf05fb31d256e63
saharahbains <saharahb@students.cs.ubc.ca>
2024-04-25 15:12:56 -0700

    edited about me

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,20 +8,37 @@ export default function AboutMe() {
         <div className="hero--section--content">
           <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section--description">
-            I am a computer science student located in Vancouver, BC that is
-            seeking Fall 2024 internships. I am currently completing a second
+            I'm a computer science student located in Vancouver, BC who is
+            seeking Fall 2024 internships. I am currently completing my second
             Bachelor's Degree in Computer Science at the University of British
             Columbia, in the BCS program. I graduated with a Bachelor of Science
             in Biology in May of 2023 and I worked in genetics research for over
             a year before discovering my love for tech. I have a passion for
-            lifelong learning and I look forward to contributing meaningfully to
-            the tech industry!{" "}
+            lifelong learning and I look forward to making meaningful
+            contributions to the tech industry!
           </p>
           <p className="hero--section--description">
             Outside of academics, I enjoy attending hackathons and building cool
             projects. In my free time you can either find me with my nose in a
             book, on a run, or in the front row at a concert! Thanks for
-            checking out my website!
+            checking out my website and feel free to contact me at_
+            <a
+              href="https://github.com/saharahb"
+              className="navbar--content"
+              target="_blank"
+              rel="noreferrer"
+            >
+              saharahb19@gmail.com
+            </a>
+            _or connect with me on_
+            <a
+              href="https://www.linkedin.com/in/saharah-bains-942290273/"
+              className="navbar--content"
+              target="_blank"
+              rel="noreferrer"
+            >
+              _LinkedIn.
+            </a>
           </p>
         </div>
       </div>
