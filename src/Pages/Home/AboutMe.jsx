57187ef805d5eabc097282da02e050be8f4e3251
saharahbains <saharahb@students.cs.ubc.ca>
2024-04-25 15:43:58 -0700

    style changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
           <h1 className="skills--section--heading">About Me</h1>
-          <p className="hero--section--description">
+          <p className="hero--section-description">
             I'm a computer science student located in Vancouver, BC who is
             seeking Fall 2024 internships. I am currently completing my second
             Bachelor's Degree in Computer Science at the University of British
@@ -17,12 +17,12 @@ export default function AboutMe() {
             lifelong learning and I look forward to making meaningful
             contributions to the tech industry!
           </p>
-          <p className="hero--section--description">
+          <p className="hero--section-description">
             Outside of academics, I enjoy attending hackathons and building cool
             projects. In my free time you can either find me with my nose in a
             book, on a run, or in the front row at a concert!
           </p>
-          <p className="hero--section--description">
+          <p className="hero--section-description">
             Thanks for checking out my website!
           </p>
         </div>
